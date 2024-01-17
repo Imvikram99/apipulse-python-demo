d42a4f109abe1e04c7c3227c55e9b99d2de7f3f3
Imvikram99 <120546792+Imvikram99@users.noreply.github.com>
2024-01-17 21:59:15 +0530

    init

---
+++ myapi/serializers.py
@@ -0,0 +1,9 @@
+# myapi/serializers.py
+
+from rest_framework import serializers
+from .models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'description']
